afa8b1c2a905f55ec1b18c20a03d96a39e81bee6
tangzhenlang2023051604014 <quan0524@qq.com>
2024-12-30 20:08:12 +0800

    tzl add student.h

---
+++ student.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
+
+class Course;
+
+class Student {
+public:
+    Student(const std::string& name, int id);
+
+    int getStudentID() const;
+    std::string getName() const;
+
+    void selectCourse(std::shared_ptr<Course> course);
+    void viewScores() const;
+    void viewCourses() const;
+    const std::vector<std::shared_ptr<Course>>& getCourses() const;
+
+private:
+    std::string name;
+    int studentID;
+    std::vector<std::shared_ptr<Course>> enrolledCourses;
+
+
+};
+
